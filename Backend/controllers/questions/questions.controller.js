42fa1235b7be92ac88a7d839ce46f2ab5b9f972c
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-05-30 02:12:46 +0530

    chore: getQuestions controller

--- Backend/controllers/questions/questions.controller.js
+++ Backend/controllers/questions/questions.controller.js
@@ -1,36 +1,16 @@
 const client = require("../../config/dbConfig");
 
-exports.getQuestions = async (req, res) => {
-  const domain_name = req.query.domain_name;
-  const level = req.query.level;
-
-  let query = `select questions.text , code_snippets.code , domains.domain_name
-  from questions q
-  inner join code_snippets c on q.id = c.question_id
-  inner join domains d on q.domain_id = d.id
-  `;
-  let values = [];
-
-  if (domain && difficulty) {
-    //if both parameters are provided , filter the results by both domain and difficulty.
-    query += `where d.name=$1 and q.difficulty =$2`;
-    values = [domain, difficulty];
-  } else if (domain) {
-    // if only the domain parameters are provided filter the results by domain only
-    query += `where d.name = $1 `;
-    values = [domain];
-  } else if (difficulty) {
-    query += `where q.difficulty = $1`;
-    values = [difficulty];
-  }
-  // add the sorting condition to the query
-  query += `order by d.name asc,q.text desc`;
-
+exports.getAllQuestions = async (req, res) => {
   try {
-    const result = await client.query(query, values);
-    res.status(200).send({ message: result.rows });
+    // Fetch all the questions from the database
+    const query = "SELECT * FROM questions";
+    const result = await client.query(query);
+
+    // Return the questions to the client
+    res.json({ data: result.rows });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    console.error(err);
+    res.status(500).json({ message: "Error retrieving questions" });
   }
 };
 
