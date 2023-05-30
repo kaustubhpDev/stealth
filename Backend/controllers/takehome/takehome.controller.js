b91dee51a7725c10e794a5f3af96625bac6c50cc
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-05-30 14:36:14 +0530

    feat: download&upload take home

--- Backend/controllers/takehome/takehome.controller.js
+++ Backend/controllers/takehome/takehome.controller.js
@@ -20,14 +20,17 @@ exports.savetakehomeassignment = async (req, res) => {
   }
 };
 
-exports.submittakehomeassignment = async (req, res) => {
+exports.downloadTakeHomeAssignment = async (req, res) => {
   try {
-    const { student_id, assignment_id, submission_url } = req.body;
+    const { student_id, assignment_id, assignment_started_date } = req.body;
+
     const insertQuery =
-      "INSERT INTO assignment_submission (user_id, assignment_id, submission_url) VALUES ($1, $2, $3) RETURNING submission_id";
-    const values = [student_id, assignment_id, submission_url];
+      "INSERT INTO assignment_submission (user_id, assignment_id, assignment_started_date) VALUES ($1, $2, $3) RETURNING submission_id";
+    const values = [student_id, assignment_id, assignment_started_date];
+
     const queryResult = await client.query(insertQuery, values);
     const newSubmissionId = queryResult.rows[0].submission_id;
+
     res.status(201).json({
       message: "Assignment submitted successfully",
       submissionId: newSubmissionId,
@@ -37,6 +40,57 @@ exports.submittakehomeassignment = async (req, res) => {
     res.status(500).json({ message: "Error saving assignment submission" });
   }
 };
+
+exports.updateAssignmentSubmission = async (req, res) => {
+  try {
+    const { student_id, assignment_id, submission_url } = req.body;
+
+    const checkQuery = `
+      SELECT assignment_started_date
+      FROM assignment_submission
+      WHERE user_id = $1 AND assignment_id = $2
+      ORDER BY assignment_started_date DESC
+      LIMIT 1
+    `;
+    const checkResult = await client.query(checkQuery, [
+      student_id,
+      assignment_id,
+    ]);
+
+    if (checkResult.rows.length === 0) {
+      return res.status(400).json({ message: "Assignment not found" });
+    }
+
+    const assignmentStartedDate = checkResult.rows[0].assignment_started_date;
+    const threeDaysAgo = new Date();
+    threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
+
+    if (assignmentStartedDate < threeDaysAgo) {
+      return res
+        .status(400)
+        .json({ message: "Assignment submission period expired" });
+    }
+
+    const updateQuery = `
+      UPDATE assignment_submission
+      SET submission_url = $1
+      WHERE user_id = $2 AND assignment_id = $3
+    `;
+    await client.query(updateQuery, [
+      submission_url,
+      student_id,
+      assignment_id,
+    ]);
+
+    res
+      .status(200)
+      .json({ message: "Assignment submission updated successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error updating assignment submission" });
+  }
+};
+
 exports.getAllAssignments = async (req, res) => {
   try {
     const result = await client.query("SELECT * FROM assignment");
--- Backend/routes/takehome.routes.js
+++ Backend/routes/takehome.routes.js
@@ -2,8 +2,12 @@ module.exports = (app) => {
   const takehomeassignment = require("../controllers/takehome/takehome.controller");
   const router = require("express").Router();
 
-  router.post("/assignment/save", function (req, res) {
-    takehomeassignment.savetakehomeassignment(req, res);
+  router.post("/assignment/download", function (req, res) {
+    takehomeassignment.downloadTakeHomeAssignment(req, res);
+  });
+
+  router.post("/assignment/upload", function (req, res) {
+    takehomeassignment.updateAssignmentSubmission(req, res);
   });
 
   router.get("/assignment/getassignments", function (req, res) {
